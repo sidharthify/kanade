77dd95cfbd90a023a2dc803ba60a6dacccc0b232
sidharthify <wednisegit@gmail.com>
2026-04-09 16:48:26 +0800

    kanade: Initial xcodeproj
    Signed-off-by: sidharthify <wednisegit@gmail.com>

---
+++ kanade/kanadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  kanadeApp.swift
+//  kanade
+//
+//  Created by user948538 on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct kanadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
